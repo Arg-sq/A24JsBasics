01632c3bd5c20f22955184194cc5cb92b6fbe709
Ashesh Gurung <ashesh.gurung@eclathealth.com>
2025-11-10 07:30:08 +0545

    feat: js basic started

---
+++ jsBasics.js
@@ -0,0 +1,13 @@
+console.log(a);
+var a = "xyz";
+
+printName();
+function printName() {
+  console.log("I am normal func");
+}
+printName2();
+const printName2 = () => {
+  console.log("I am arrow func");
+};
+
+// console.log(a);
